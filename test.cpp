fa09b5fed9e69efba24dbb4d0027af4d60338251
Nguyen Viet Anh <blueace99@gmail.com>
2018-02-01 16:30:57 +0700

    khoi tao chuong trinh test.cpp

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Enter name: "<<endl;
+	cin>>names>>endl;
+	cout<<"hello,  "<<names<<endl;
+	if(name.length()  >25);
+	{
+		cout<<"You have    !"<<endl;
+
+	}
+
+
+	return 0;
+}
